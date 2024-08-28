118ddf1c82bad3dda7278b5cd325a816277db740
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-29 00:24:11 +0600

    Add files via upload

---
+++ PHP-AND-MySQL-Novice-to-Ninja/Registration-Validation-Email2/phpmysql7-Registration-Validation-Email2/Ijdb/JokeWebsite.php
@@ -0,0 +1,26 @@
+<?php
+namespace Ijdb;
+class JokeWebsite implements \Ninja\Website {
+    public function getDefaultRoute() {
+        return 'joke/home';
+    }
+
+    public function getController(string $controllerName):? object {
+        $pdo = new \PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+
+        $jokesTable = new \Ninja\DatabaseTable($pdo, 'joke', 'id');
+        $authorsTable = new \Ninja\DatabaseTable($pdo, 'author', 'id');
+
+        if ($controllerName === 'joke') {
+            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable);
+        }
+        else if ($controllerName === 'author') {
+            $controller = new \Ijdb\Controllers\Author($authorsTable);
+        }
+        else {
+            $controller = null;
+        }
+
+        return $controller;
+    }
+}
