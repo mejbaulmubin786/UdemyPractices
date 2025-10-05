e33e73d144a09c97a1bbd1d35e23f51f59730165
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-06 03:32:46 +0600

    first project initial work is done

--- JS/DOM/GuessMyNumber/script.js
+++ JS/DOM/GuessMyNumber/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let Highscore = 0;
+
+let number = document.querySelector('.number');
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+
+document.querySelector('.check').addEventListener('click', function(){
+displayMessage("Chick the button");
+});
+
+
