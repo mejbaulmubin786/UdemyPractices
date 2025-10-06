5e1782b69370af96cbc78fe2703ceac956410863
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-06 13:10:26 +0600

    update something

--- JS/DOM/GuessMyNumber/script.js
+++ JS/DOM/GuessMyNumber/script.js
@@ -2,17 +2,29 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let Highscore = 0;
 
-let number = document.querySelector('.number');
+function message(message){
+document.querySelector('.message').textContent=message;
+}
 
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-
+console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function(){
-displayMessage("Chick the button");
+
+let gessNumber = Number(document.querySelector('.guess').value); 
+
+if(!gessNumber){
+  message("Please enter a valid number between 1 to 20");
+} else if(gessNumber===secretNumber){
+  message("Congratulation Well Done!!!");
+  document.querySelector("body").style.backgroundColor = "green";
+  document.querySelector(".number").textContent = secretNumber;
+}else if(gessNumber>secretNumber){
+  message("Too big");
+  score--;
+}else {
+  message("Too small");
+  score--;
+}
+ document.querySelector(".score").textContent = score;
 });
-
-
